3103254656ae8d85cd03d8cd3348df344b960660
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-06 19:26:00 +0300

    feat(controller): add validation of data and path selection on intro page

---
+++ src/main/java/com/example/handwritinggeneratorutility/controller/IntroController.java
@@ -0,0 +1,105 @@
+package com.example.handwritinggeneratorutility.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class IntroController {
+    private Stage stage;
+
+    @FXML
+    protected Label err_message;
+
+    @FXML
+    protected TextField tf_width;
+
+    @FXML
+    protected TextField tf_height;
+
+    @FXML
+    protected TextField tf_path;
+
+    @FXML
+    protected Button btn_select_path;
+
+    @FXML
+    protected Button btn_open_canvas;
+
+//    IntroController() {}
+
+    public void setDefault(Stage stage) {
+        err_message.setVisible(false);
+    }
+
+    private boolean isWidthValid() {
+        String data = tf_width.getCharacters().toString();
+        try {
+            int width = Integer.parseInt(data);
+            return width > 0;
+        } catch (NumberFormatException _) {
+            return false;
+        }
+    }
+
+    private boolean isHeightValid() {
+        String data = tf_height.getCharacters().toString();
+        try {
+            int height = Integer.parseInt(data);
+            return height > 0;
+        } catch (NumberFormatException _) {
+            return false;
+        }
+    }
+
+    private boolean isPathValid() {
+        String path = tf_path.getCharacters().toString();
+        File dir = new File(path);
+        return dir.exists();
+    }
+
+    @FXML
+    protected void pathSelect() {
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setTitle("Выбор папки");
+        File enteredPath = new File(tf_path.getCharacters().toString());
+        if(enteredPath.exists()) dirChooser.setInitialDirectory(enteredPath);
+        File selectedDir = dirChooser.showDialog(this.stage);
+        if(selectedDir != null) tf_path.setText(selectedDir.getPath());
+    }
+
+    @FXML
+    protected void openCanvas() {
+        err_message.setVisible(false);
+
+        if(tf_width.getCharacters().isEmpty()
+                || tf_height.getCharacters().isEmpty()
+                || tf_path.getCharacters().isEmpty()) {
+            err_message.setText("Все поля должны быть заполнены!");
+            err_message.setVisible(true);
+            return;
+        }
+
+        if(!this.isWidthValid()) {
+            err_message.setText("Ширина холста должна быть целым положительным числом!");
+            err_message.setVisible(true);
+            return;
+        }
+        if(!this.isHeightValid()) {
+            err_message.setText("Высота холста должна быть целым положительным числом!");
+            err_message.setVisible(true);
+            return;
+        }
+        if(!this.isPathValid()) {
+            err_message.setText("Указанный путь должен соответствовать реальной папке!");
+            err_message.setVisible(true);
+            return;
+        }
+
+        // TODO: creating instance of Session and bundling with SessionController
+    }
+}
