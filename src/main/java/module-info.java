e49993e39680838f47dd2fd25f340be317a7b192
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-06 19:29:54 +0300

    build(whole): update configuration files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module com.example.handwritinggeneratorutility {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.compiler;
 
     opens com.example.handwritinggeneratorutility to javafx.fxml;
     exports com.example.handwritinggeneratorutility;
+    exports com.example.handwritinggeneratorutility.controller;
+    opens com.example.handwritinggeneratorutility.controller to javafx.fxml;
 }
