a95bf04a33e1bc756472a8bb5e5666f9a2a5424a
Honsage <musatov.i.a@edu.mirea.ru>
2025-01-07 16:31:03 +0300

    feat(model): add class describing configuration of generator

---
+++ src/main/java/com/example/handwritinggeneratorutility/model/GeneratorConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.handwritinggeneratorutility.model;
+
+public class GeneratorConfiguration {
+    private int width;
+    private int height;
+    private String path;
+
+    public GeneratorConfiguration(int width, int height, String path) {
+        this.width = width;
+        this.height = height;
+        this.path = path;
+    }
+
+    public int getWidth() { return this.width; }
+
+    public int getHeight() { return this.height; }
+
+    public String getPath() { return this.path; }
+}
